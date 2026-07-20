1fa6592aac9cb17c43f82e47d3f07f2cd82ffcf0
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2026-07-20 14:55:54 -0700

    sp abund fig

--- rcode/changeSpAbund.R
+++ rcode/changeSpAbund.R
@@ -1,7 +1,7 @@
 # Started Feb 5, 2026 by D. Loughnan
 
 # aim of this code is to model changes in the abundance of the 
-# 10 most abundant and ecoligcally importnat spp
+# 13 most abundant and ecoligcally importnat spp
 
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
@@ -11,6 +11,7 @@ require(rstan)
 require(shinystan)
 require(bayesplot)
 require(brms)
+require(ggplot2)
 
 setwd("~/Documents/github/Loughnan_m2m")
 
@@ -21,10 +22,14 @@ domSp <- c("Amphissa","Ampithoidae","Aoroides","Caprella","Exogone","Harpacticoi
 
 subAbund <- abund[abund$taxonL %in% domSp,]
 
+subAbund <- subset(subAbund, site != "goose_north")
+
 abundData <- list(y = subAbund$abundance,
                     N = nrow(subAbund),
                     Nspp = length(unique(subAbund$taxonL)),
-                    species = as.numeric(as.factor(subAbund$taxonL)),
+                    Nsite = length(unique(subAbund$site)),
+                    species = as.integer(as.factor(subAbund$taxonL)),
+                    site = as.integer(as.factor(subAbund$site)),
                     year = subAbund$year-2014
                   )
 
@@ -33,7 +38,7 @@ mdlA <- stan("stan/spAbund.stan",
                    iter = 4000, warmup = 3000, chains=4,
                    include = FALSE, pars = c("y_hat"))
             #, control = list(max_treedepth = 12, adapt_delta = 0.99))
-save(mdlA, file = "output/mdlAbundChange.Rdata")
+save(mdlA, file = "output/mdlAbundChangeSite.Rdata")
 
 sumA <- summary(mdlA)$summary
 y <- subAbund$abundance
@@ -43,7 +48,7 @@ y.ext <- post$y_rep
 
 # pdf(file = "figures/mdl_densityplot_temp.pdf", width = 4, height = 4)
 # par(mfrow = c(1,2))
-ppc_dens_overlay(y, y.ext[1:500, ])
+ppc_dens_overlay(y, y.ext[1:1000, ])
 
 # dev.off()
 
@@ -54,15 +59,42 @@ quantile2575 <- function(x){
 }
 
 
-bSp <- data.frame(sumA[grep("beta_group\\[", rownames(sumA)), c("mean","2.5%", "97.5%")])
+bSp <- data.frame(sumA[grep("beta_group\\[", rownames(sumA)), c("mean")])
 aSp <- data.frame(sumA[grep("alpha_group\\[", rownames(sumA)), c("mean","2.5%", "97.5%")])
+aSite <- data.frame(sumA[grep("alpha_groupS\\[", rownames(sumA)), c("mean","2.5%", "97.5%")])
+
 quant <- t(apply(data.frame(post), 2, quantile2575) )
 
 mdlOut <- data.frame(species = sort(unique(subAbund$taxonL)))
 mdlOut$bSp <- bSp[,"mean"]
 mdlOut$aSp <- aSp[,"mean"]
-ggplot(subAbund, aes(x = year, y = abundance)) + geom_point() + facet_wrap(~taxonL)
+ggplot(subAbund, aes(x = year, y = abundance)) + geom_point() + 
+  facet_wrap(~taxonL, scales = "free")
 
+# mu plot of species:
+fitSp <- data.frame(post$beta_group)
+
+bSpMean <- (data.frame(colMeans(fitSp)))
+quantSpM <- t(apply(fitSp, 2, quantile2575) )
+bSlopes <- data.frame(cbind(bSpMean, quantSpM)); names(bSlopes) <- c("mean","X5", "X25", "X75", "X95")
+bSlopes$spId <- domSp
+
+spAbund <- ggplot()  +
+  geom_pointrange(bSlopes, mapping = aes(x = factor(spId, level = domSp), y = mean, ymin=X5, ymax= X95),
+                  size =0.55, position=position_dodge(width=0.5), color = "maroon") +
+  geom_linerange( bSlopes, mapping = aes(x = factor(spId, level = domSp), ymin = X25, ymax= X75), 
+                  size =1.25, position=position_dodge(width=0.5), color = "maroon") +
+  xlab("Dominant epifauna taxa") + ylab("Change in abundance per quadrat") +
+ # annotate("text", 1 ,1, label = "a)", size = 5.5) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        axis.text = element_text(size = 10), axis.title = element_text(size = 12),axis.text.x = element_text( size= 10,angle = 55, hjust=1), legend.key=element_rect(fill="white"),legend.text=element_text(size=25)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black")
+
+
+pdf("figures/changeAbunda.pdf", width = 6, height = 4)
+spAbund
+dev.off()
 ########################
 # how many rare spp are there?
 
--- stan/spAbund.stan
+++ stan/spAbund.stan
@@ -2,6 +2,9 @@ data {
   int<lower=0> N;
   int<lower=0> Nspp;
 array[N] int<lower=1, upper=Nspp> species;
+
+int<lower=0> Nsite;
+array[N] int<lower=1, upper=Nsite> site;
 vector[N] year;
  array[N] int<lower=0> y;
 }
@@ -13,13 +16,17 @@ parameters {
 
   // Between-group variation (Standard Deviations)
   real<lower=0> sigma_alpha;
+  real<lower=0> sigma_alphaS;
+
   real<lower=0> sigma_beta;
 
   // Group-level parameters defined DIRECTLY
   vector[Nspp] alpha_group;
+    vector[Nsite] alpha_groupS;
+
   vector[Nspp] beta_group;
 
-  // Negative Binomial overdispersion
+  // Negative Binomial 
   real<lower=0> phi;
 }
 
@@ -28,23 +35,25 @@ model {
   alpha_global ~ normal(0, 5);
   beta_global ~ normal(0, 2);
   sigma_alpha ~ exponential(1);
+  sigma_alphaS ~ exponential(1);
   sigma_beta ~ exponential(1);
   phi ~ exponential(1);
 
   // 2. Centered Distribution for Group Effects
   // These parameters are "centered" on the global mean and sigma
   alpha_group ~ normal(alpha_global, sigma_alpha);
+  alpha_groupS ~ normal(alpha_global, sigma_alphaS);
   beta_group ~ normal(beta_global, sigma_beta);
 
   // 3. Likelihood
   // Using vectorization for efficiency
-  y ~ neg_binomial_2_log(alpha_group[species] + beta_group[species] .* year, phi);
+  y ~ neg_binomial_2_log(alpha_group[species] +alpha_groupS[site] + beta_group[species] .* year, phi);
 }
 
 generated quantities {
  array[N] int y_rep;
   for (n in 1:N) {
-    real mu = exp(alpha_group[species[n]] + beta_group[species[n]] * year[n]);
+    real mu = exp(alpha_group[species[n]] + alpha_groupS[site[n]] + beta_group[species[n]] * year[n]);
     y_rep[n] = neg_binomial_2_rng(mu, phi);
   }
 }
